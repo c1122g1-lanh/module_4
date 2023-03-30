40f21dd8900f8cd4f976e0dac743d8b7a13adb40
Lanh <lanh7108@gmail.com>
2023-03-30 09:45:14 +0700

    up bai tap ss3

--- ss_3/mailbox/src/main/java/com/codegym/exercise_1/controller/MailboxController.java
+++ ss_3/mailbox/src/main/java/com/codegym/exercise_1/controller/MailboxController.java
@@ -2,21 +2,19 @@ package com.codegym.exercise_1.controller;
 
 import com.codegym.exercise_1.model.Mailbox;
 import com.codegym.exercise_1.service.impl.MailboxService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MailboxController {
-    @Autowired
-    private MailboxService mailboxService;
-    String[] languages = new String[]{"English", "Vietnamese", "Japanese", "Chinese"};
-    Integer[] pageSize = new Integer[]{5, 10, 15, 25, 50, 100};
+
+    private MailboxService mailboxService = new MailboxService();
+    String[] languages = mailboxService.languages();
+    Integer[] pageSize = mailboxService.pageSize();
 
     @GetMapping("form1")
     public String showFormUpdate(Model model) {
--- ss_3/mailbox/src/main/java/com/codegym/exercise_1/service/impl/MailboxService.java
+++ ss_3/mailbox/src/main/java/com/codegym/exercise_1/service/impl/MailboxService.java
@@ -2,13 +2,12 @@ package com.codegym.exercise_1.service.impl;
 
 import com.codegym.exercise_1.repository.impl.MailboxRepository;
 import com.codegym.exercise_1.service.IMailboxService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MailboxService implements IMailboxService {
-    @Autowired
-    private MailboxRepository mailboxRepository;
+
+    private MailboxRepository mailboxRepository = new MailboxRepository();
 
     @Override
     public String[] languages() {
